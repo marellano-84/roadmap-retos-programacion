f85b94ef6b2cba2f650901491debf8eed887aafc
Mau Arellano <mau.arellano@outlook.com>
2024-09-10 19:51:38 -0600

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/marellano-84.py
@@ -0,0 +1,29 @@
+# https://www.python.org/
+
+# Comentario en una lines 
+
+"""
+Esto es un comentario
+usando varias lineas
+de texto
+"""
+
+'''
+Esto es un comentario
+usando varias lineas
+de texto
+'''
+
+my_variable = "Soy una variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #por convención (python no tiene constantes)
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("Hola, Python!")
