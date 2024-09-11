276a9998d7ce46d2300eabcee419d105cd97c900
Mau Arellano <mau.arellano@outlook.com>
2024-09-11 03:42:23 -0600

    Correccion de variable en ciclo 'while' y ejercicio extra

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/marellano-84.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/marellano-84.py
@@ -94,6 +94,6 @@ finally:
 Ejercicio Extra
 """
 
-"""for number in range(10, 56):
+for number in range(10, 56):
     if number % 2 == 0 and number != 16 and number % 3 != 0:
-        print(number)"""
+        print(number)
